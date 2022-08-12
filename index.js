aff972ca85772baa6974200c9500652325b0b0e6
Pink Shark <esipovich2003@gmail.com>
2022-08-13 00:21:47 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+let inp = document.querySelector('.inp')
+const but = document.querySelector('.but')
+
+but.addEventListener('click' , () => { 
+    let inp = (document.querySelector('.inp').value)
+    alert(inp + ' ' + 'Lox') } )
